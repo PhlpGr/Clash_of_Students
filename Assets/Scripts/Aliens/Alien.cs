16d572ac20f0a2b9129bbcdd05e3304875bf2cc2
Teschner <Magnus.Teschner@thieme.de>
2024-08-19 21:45:47 +0200

    Add Alien script and add dynamic properties to Alien objects in level2

---
+++ Assets/Scripts/Aliens/Alien.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien1 : MonoBehaviour
+{
+    public int Level;
+    public int Position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Level = 1;
+        Position = 1;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
